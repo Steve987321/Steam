d4920079a7b8792a68e5e4d81a240aa26b950dcc
vierkant <Stevebudding123@gmail.com>
2023-12-04 10:43:40 +0100

    Correcte naam voor Steam API call

--- Code/APICalls.py
+++ Code/APICalls.py
@@ -7,7 +7,7 @@ class SteamAPI:
     # TODO: Ergens anders opslaan?
     KEY = "0CF13F9FBA5C093B11239F396170BD4D"
 
-    def GetPlayerSummary(id: str | list[str]):
+    def GetPlayersSummaries(id: str | list[str]):
         """
         Geeft informatie van de speler/profiel door de gegeven SteamID.
 
@@ -16,13 +16,6 @@ class SteamAPI:
         Returns:
             ....
         """
-
-        request = f"http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key={SteamAPI.KEY}&steamids={id}"
+        request = f"https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key={SteamAPI.KEY}&steamids={id}"
         response = requests.get(request)
-        print(response)
-        data = response.text
-        data2 = json.loads(data)
-
-        # req2 = f"https://api.steampowered.com/ISteamUser/GetFriendList/v1/?key={SteamAPI.KEY}&steamid=76561198123041058&format=json"
-
-        return data2
+        return json.loads(response.text)
--- Code/main.py
+++ Code/main.py
@@ -2,5 +2,5 @@ from APICalls import SteamAPI
 
 
 if __name__ == "__main__":
-    a = SteamAPI.GetPlayerSummary("76561198123041058")
+    a = SteamAPI.GetPlayersSummaries("76561198123041058")
     print(a)
