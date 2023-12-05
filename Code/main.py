6c6b225679fa5fed8bff90a84555ffeff3ac6b5c
vierkant <Stevebudding123@gmail.com>
2023-12-05 14:13:55 +0100

    namen veranderd en andere return value voor get_steamid_name

--- Code/main.py
+++ Code/main.py
@@ -9,17 +9,21 @@ def test_scherm():
 
 def test_steam_api():
     # voeg testen toe
-    for i in SteamAPI.get_player_ID('76561198123041058'):
-        a = SteamAPI.get_players_summaries(i)
-        print(a)
+    friends = SteamAPI.get_player_friendsid('76561198123041058')
+    friends_names = SteamAPI.get_steamid_name(friends)
+    print(friends_names)
 
 
 def test_steamnaam():
-    a = SteamAPI.get_player_ID('76561198123041058')
+    a = SteamAPI.get_steamid_name(['76561198123041058', "76561198181797231"])
+    print(a)
+    a = SteamAPI.get_steamid_name(['76561198123041058'])
+    print(a)
+    a = SteamAPI.get_steamid_name('76561198123041058')
     print(a)
 
 
 if __name__ == "__main__":
     # test_scherm()
-    # test_steamnaam()
+    test_steamnaam()
     test_steam_api()
