e4f417566ecf5c187c0a419bd7e2fe5df0aa60e9
DanielBekkers <dct.bekkers@gmail.com>
2023-12-07 11:38:32 +0100

    Two functions: - filter all keys - filter only keys we decide

--- Code/Sort_json_file_by_choice.py
+++ Code/Sort_json_file_by_choice.py
@@ -1,9 +1,7 @@
 import json
 
-# NOTE!!!! Werkt nu niet omdat ik niet het json goede bestand in open() heb gedaan! Het is beter om dit the testen met een kleiner json bestand
 
-
-def filter_list():
+def filter_list_all_keys():
     with open("test.json") as file_read:
         rjson = file_read.read()
         loaded_json = json.loads(rjson)
@@ -17,17 +15,56 @@ def filter_list():
         # Check if input is correct
         correct_input = False
         input_key = ""
+        rev = True
         while not correct_input:
-            key_input = input(f"\x1b[34mFilter by one of the following: {key_string}\nFilter by: ")
+            key_input = input(
+                f"\x1b[34mFilter by one of the following: {key_string}\nFilter by (key, desc (optional)): ")
 
             for key in keys:
                 if key_input == key:
                     correct_input = True
                     input_key = key
+                elif key_input == key + ", desc":
+                    correct_input = True
+                    input_key = key
+                    rev = False
 
         print(f"\x1b[31m{loaded_json}")
-        sorted_list = sorted(loaded_json, key=lambda x: x[input_key], reverse=True)  # lambda function gets the values of dict
+        sorted_list = sorted(loaded_json, key=lambda x: x[input_key], reverse=rev)  # lambda function gets the values of dict
         print(f"\x1b[32m{sorted_list}")
 
 
+def filter_list():
+    with open("test.json") as file_read:
+        rjson = file_read.read()
+        loaded_json = json.loads(rjson)
+
+        # Get the keys
+        keys = ["name", "release_date", "required_age", "achievements", "positive_ratings", "negative_ratings", "average_playtime", "owners", "price"]
+        key_string = ""
+        for key in keys:
+            key_string += f"{key}, "
+
+        # Check if input is correct
+        correct_input = False
+        input_key = ""
+        rev = True
+        while not correct_input:
+            key_input = input(f"\x1b[34mFilter by one of the following: {key_string}\nFilter by (key, asc (optional)): ")
+
+            for key in keys:
+                if key_input == key:
+                    correct_input = True
+                    input_key = key
+                elif key_input == key + ", asc":
+                    correct_input = True
+                    input_key = key
+                    rev = False
+
+        print(f"\x1b[31m{loaded_json}")
+        sorted_list = sorted(loaded_json, key=lambda x: x[input_key], reverse=rev)  # lambda function gets the values of dict
+        print(f"\x1b[32m{sorted_list}")
+
+
+# filter_list_all_keys()
 filter_list()
