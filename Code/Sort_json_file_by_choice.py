9a6853ba375677da599bd92a51586d9cdd6efca7
DanielBekkers <dct.bekkers@gmail.com>
2023-12-07 15:13:16 +0100

    Reversed function without using reversed() Deleted filter_all_keys() function --> was not needed

--- Code/Sort_json_file_by_choice.py
+++ Code/Sort_json_file_by_choice.py
@@ -12,42 +12,11 @@ def bubble_sort(lst, key, descending=False):
         length_of_unsorted_list -= 1
 
     if descending:
-        return list(reversed(lst))
-
+        rev_list = []
+        for i in lst:  # My own reversed function, instead of reversed(lst)
+            rev_list.insert(0, i)
+        return rev_list
     return lst
-
-
-def filter_list_all_keys():
-    with open("test.json") as file_read:
-        rjson = file_read.read()
-        loaded_json = json.loads(rjson)
-
-        # Get the keys
-        keys = loaded_json[0].keys()
-        key_string = ""
-        for key in keys:
-            key_string += f"{key}, "
-
-        # Check if input is correct
-        correct_input = False
-        input_key = ""
-        rev = True
-        while not correct_input:
-            key_input = input(
-                f"\x1b[34mFilter by one of the following: {key_string}\nFilter by (key, desc (optional)): ")
-
-            for key in keys:
-                if key_input == key:
-                    correct_input = True
-                    input_key = key
-                elif key_input == key + ", desc":
-                    correct_input = True
-                    input_key = key
-                    rev = False
-
-        print(f"\x1b[31m{loaded_json}")
-        sorted_list = sorted(loaded_json, key=lambda x: x[input_key], reverse=rev)  # lambda function gets the values of dict
-        print(f"\x1b[32m{sorted_list}")
 
 
 def filter_list():
@@ -56,7 +25,7 @@ def filter_list():
         loaded_json = json.loads(rjson)
 
         # Get the keys
-        keys = ["name", "release_date", "required_age", "achievements", "positive_ratings", "negative_ratings", "average_playtime", "owners", "price"]
+        keys = ["appid", "name", "release_date", "required_age", "achievements", "positive_ratings", "negative_ratings", "average_playtime", "owners", "price"]
         key_string = ""
         for key in keys:
             key_string += f"{key}, "
@@ -82,5 +51,4 @@ def filter_list():
         print(f"\x1b[32m{sorted_list}")
 
 
-# filter_list_all_keys()
 filter_list()
