3402f19f994e579b92e0377678f3280d6a3812da
duncan <duncan.reijersen@student.hu.nl>
2023-12-18 10:08:38 +0100

    testing class

--- Code/GUI.py
+++ Code/GUI.py
@@ -1,15 +1,12 @@
 import customtkinter as ctk
+from main import ApiLoop
+
+
 
 
 class Window:
-    def __init__(self,
-                 naam: str,
-                 win_width: int,
-                 win_height: int):
-
-        # App appearance
+    def __init__(self, naam: str, win_width: int, win_height: int):
         ctk.set_appearance_mode("System")
-        # Kleuren van de Widget
         ctk.set_default_color_theme("green")
 
         self.root = ctk.ctk_tk.CTk()
@@ -18,7 +15,14 @@ class Window:
         self.root.geometry(f"{win_width}x{win_height}")
         self.root.title(naam)
 
-        ctk.CTkButton(self.root, text="knop").pack()
+        button = ctk.CTkButton(self.root, text="knop", command=self.button_click)
+        button.pack()
+
+    def button_click(self):
+        api_loop_instance = ApiLoop()
+        api_loop_instance.test_steam_api()
 
     def show(self):
         self.root.mainloop()
+
+
--- Code/main.py
+++ Code/main.py
@@ -22,10 +22,10 @@ with open('steamid.txt', 'r+') as file:
 #        api key aan vragen voor onthoud
 
 
+
 def test_scherm():
     scherm = Window("Steam Train Groep", 500, 500)
     scherm.show()
-
 
 import time
 
@@ -36,55 +36,43 @@ previous_game_info = {}
 previous_ingame_info = {}
 
 
-def test_steam_api():
-    friends = SteamAPI.get_player_friendsid(steam_id)
-    friends_statusses = SteamAPI.get_player_states(friends)
-    online_friends = [sid for sid, status in friends_statusses.items() if status == SteamAPI.PlayerStatus.ONLINE]
+class ApiLoop:
 
-    global previous_online_friends, previous_friends_names, previous_game_info, previous_ingame_info
+    def test_steam_api(self):
+        while True:
+            friends = SteamAPI.get_player_friendsid(steam_id)
+            friends_statusses = SteamAPI.get_player_states(friends)
+            online_friends = [sid for sid, status in friends_statusses.items() if
+                              status == SteamAPI.PlayerStatus.ONLINE]
 
-    if (
-            set(online_friends) != previous_online_friends or
-            set(friends) != previous_friends_names
-    ):
-        # Print online friends and their names
-        #print("Online friends:", online_friends)
-        friends_names = SteamAPI.get_steamid_name(friends)
-        #print("Friends names:", friends_names)
+            global previous_online_friends, previous_friends_names, previous_game_info, previous_ingame_info
 
-        game_info = SteamAPI.get_player_game(online_friends)
-        if game_info != previous_game_info:
-            print("Game information:", game_info)
+            if (
+                    set(online_friends) != previous_online_friends or
+                    set(friends) != previous_friends_names
+            ):
+                # Print online friends and their names
+                # print("Online friends:", online_friends)
+                friends_names = SteamAPI.get_steamid_name(friends)
+                # print("Friends names:", friends_names)
 
-        ingame_info = SteamAPI.check_playerfriends_ingame(online_friends, steam_id)
-        #print(ingame_info)
-        if ingame_info != previous_ingame_info:
-            print("In-game information:", ingame_info)
+                game_info = SteamAPI.get_player_game(online_friends)
+                if game_info != previous_game_info:
+                    print("Game information:", game_info)
 
-        previous_online_friends = set(online_friends)
-        previous_friends_names = set(friends)
-        previous_game_info = game_info
-        previous_ingame_info = ingame_info
+                ingame_info = SteamAPI.check_playerfriends_ingame(online_friends, steam_id)
+                # print(ingame_info)
+                if ingame_info != previous_ingame_info:
+                    print("In-game information:", ingame_info)
 
-    time.sleep(10)
-    return test_steam_api()
+                previous_online_friends = set(online_friends)
+                previous_friends_names = set(friends)
+                previous_game_info = game_info
+                previous_ingame_info = ingame_info
 
+            time.sleep(10)
 
-
-
-
-# def test_steamnaam():
-#     a = SteamAPI.get_steamid_name(steam_id)
-#     print(a)
-#     a = SteamAPI.get_player_summary(steam_id)
-#     print(a)
-#     a = SteamAPI.get_player_game(SteamAPI.get_player_friendsid(steam_id))
-#     print(a)
-#     a = SteamAPI.check_playerfriends_ingame(SteamAPI.get_player_friendsid(steam_id))
-#     print(a)
-
-
-if __name__ == "__main__":
-    #test_steamnaam()
-    test_steam_api()
-    #test_scherm()
+# if __name__ == "__main__":
+#     #test_steamnaam()
+#     #test_steam_api()
+#     test_scherm()
