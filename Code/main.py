1bc401fd5ff4a6f6aff3dce7e962f87c5fc97e31
DamianRHU <144605413+DamianHUg@users.noreply.github.com>
2024-01-31 20:35:26 +0100

    Update main.py

--- Code/main.py
+++ Code/main.py
@@ -1,35 +1,3 @@
-# from SteamAPI import api
-# import time
-# from GUI import Window
-# import threading
-# # from lcd_led_pico_code import pico_main
-#
-# with open('steamid.txt', 'r+') as file:
-#     check = file.readlines()
-#     if not check:
-#         steam_id = input('What is your steamID? ')
-#         file.write(steam_id)
-#     else:
-#         steam_id = check[0].strip()
-#         #print(steam_id)
-#
-# def test_scherm():
-#     scherm = Window("Steam Train Groep", 500, 500, steam_id)
-#     scherm.show()
-#
-# def test_Steamapi():
-#     api
-#
-# if __name__ == "__main__":
-#     #test_steamnaam()
-#     t1 = threading.Thread(target=test_scherm,)
-#     t2 = threading.Thread(target=test_Steamapi,)
-#
-#     t1.start()
-#     t2.start()
-#
-#     t1.join()
-#     t2.join()
 import SteamAPI
 import time
 from GUI import Window
